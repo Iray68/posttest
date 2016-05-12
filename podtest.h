6ecc7bdfc2d55f9fa3615b322075fc5c28418252
yiray <yiray@anniedeMacBook-Air.local>
2016-05-12 09:49:40 +0800

    podtest start

---
+++ podtest.h
@@ -0,0 +1,13 @@
+//
+//  podtest.h
+//  podtestPoject
+//
+//  Created by yiray on 2016/5/12.
+//  Copyright © 2016年 yiray. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface podtest : NSObject
+
+@end
---
+++ podtest.m
@@ -0,0 +1,19 @@
+//
+//  podtest.m
+//  podtestPoject
+//
+//  Created by yiray on 2016/5/12.
+//  Copyright © 2016年 yiray. All rights reserved.
+//
+
+#import "podtest.h"
+
+@implementation podtest
+{
+    
+}
+
+-(void)hellotest{
+    NSLog(@"hello, pods");
+}
+@end
